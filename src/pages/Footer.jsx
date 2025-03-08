7489da019e2067d40c5773e30a2a253b74a6776c
Matias Kippes <matiaskippes99@gmail.com>
2025-03-08 16:14:12 -0300

    Footer logo and text

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,44 +3,44 @@ import React from 'react';
 const ComponentName = () => {
   return (
     <footer className="bg-black text-white py-4">
-    <div className="container mx-auto flex flex-col items-center gap-4">
-      {/* Logo */}
-      <img
-        src="/erickgomez.png"
-        alt="Erick Gómez Logo"
-        className="w-[200px] mt-[-20px] "
-      />
-      
-      {/* Texto principal */}
-      <p className="text-center text-md font-bold mt-[-50px]">
-        Erick Gómez Academy 2019®️ | Todos los derechos reservados ©️
-      </p>
-      
-      {/* Términos */}
-      <p className="text-center text-sm">
-        <a
-          href="/politicas-de-privacidad"
-          className="hover:underline"
-        >
-          Políticas de privacidad
-        </a>
-        {" | "}
-        <a
-          href="/terminos-condiciones"
-          className="hover:underline"
-        >
-          Términos de condiciones y uso
-        </a>
-        {" | "}
-        <a
-          href="/politica-compra"
-          className="hover:underline"
-        >
-          Política de compra
-        </a>
-      </p>
-    </div>
-  </footer>
+      <div className="container mx-auto flex flex-col items-center gap-4">
+        {/* Logo */}
+        <img
+          src="/LOGO1.png"
+          alt="Dario Costanza Logo"
+          className="w-[150px] mt-[0px] mb-[45px]"
+        />
+
+        {/* Texto principal */}
+        <p className="text-center text-md font-bold mt-[-50px]">
+          Dario Costanza 2025®️ | Todos los derechos reservados ©️
+        </p>
+
+        {/* Términos */}
+        <p className="text-center text-sm">
+          <a
+            href="/politicas-de-privacidad"
+            className="hover:underline"
+          >
+            Políticas de privacidad
+          </a>
+          {" | "}
+          <a
+            href="/terminos-condiciones"
+            className="hover:underline"
+          >
+            Términos de condiciones y uso
+          </a>
+          {" | "}
+          <a
+            href="/politica-compra"
+            className="hover:underline"
+          >
+            Política de compra
+          </a>
+        </p>
+      </div>
+    </footer>
   );
 };
 
