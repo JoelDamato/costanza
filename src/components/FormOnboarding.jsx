48c56d2cb8363f468b7c96d85f08ea80805cecb0
Joel Damato <damatojoel25@gmail.com>
2025-05-14 21:50:29 -0300

    add

--- src/components/FormOnboarding.jsx
+++ src/components/FormOnboarding.jsx
@@ -30,7 +30,7 @@ const FormOnboarding = () => {
 
   const API_BASE_URL =
     process.env.NODE_ENV === "production"
-      ? "https://back-cursos.onrender.com"
+      ? 'https://back-cos-gim3.onrender.com'
       : "http://localhost:5000";
 
   const email = localStorage.getItem("email") || "";
